c9c2f403f5afa0a90bf36180cbba7ef704d41463
ItayAvrahami <itayavrahami1@gmail.com>
2020-10-10 16:42:06 +0300

    FIRST UPLOAD

---
+++ src/views/WhosWatch.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <section>
+    <h2>Who's Watching</h2>
+    <UsersList :users="users" @chooseUser="setUser" />
+    <router-link class="clean-link" :to="`/edit`"><button class="add-user-btn">Add User </button></router-link>
+  </section>
+</template>
+
+<script>
+import { userService } from "@/services/userService";
+import UsersList from "@/cmps/UsersList.vue";
+
+export default {
+  data() {
+    return {
+      users: undefined,
+    };
+  },
+  async created() {
+    this.users = await userService.query();
+  },
+  methods: {
+    setUser(user) {
+      console.log("user", user);
+    },
+  },
+  components: {
+    UsersList,
+  },
+};
+</script>
